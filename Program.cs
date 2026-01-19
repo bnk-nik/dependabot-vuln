6188dc9828edec3be9dea7b9a60c2930f15f7664
Bopanna <70202018+bnk-nik@users.noreply.github.com>
2026-01-19 14:55:32 +0530

    Add basic Program.cs file

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace VulnerableApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello from a vulnerable .NET app!");
+        }
+    }
+}
